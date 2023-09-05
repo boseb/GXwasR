2979b748bd935816bd72dca3ee645d2c24a1325c
Banabithi Bose <33611051+boseb@users.noreply.github.com>
2023-09-05 12:30:38 -0500

    Add files via upload

---
+++ R/datasets.R
@@ -0,0 +1,103 @@
+#' Example of covariate dataframe
+#'
+#' @name Example_covarfile
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example of ancestry dataframe of samples
+#'
+#' @name example_data_study_sample_ancestry
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for h2 estimation between male and female for eight different traits
+#'
+#' @name Example_h2data
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for phenotype file
+#'
+#' @name Example_phenofile
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset with p-value threshold
+#'
+#' @name Example_pthresoldfile
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for running DiffZerOne function
+#'
+#' @name Example_rgdata
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for MaleWAS
+#'
+#' @name Mfile
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for FemaleWAS
+#'
+#' @name Ffile
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for regression
+#'
+#' @name Regression_Ex
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for GWAS summary statistics
+#'
+#' @name Summary_Stat_Ex1
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for GWAS summary statistics
+#'
+#' @name Summary_Stat_Ex2
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for XWAS summary statistics
+#'
+#' @name XWAS_Summary_Example
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
+
+#' Example dataset for high LD regions in hg19 built
+#'
+#' @name highLD_hg19
+#' @docType data
+#' @author Banabithi Bose
+#' @keywords data
+NULL
