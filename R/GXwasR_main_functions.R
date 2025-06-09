eade241f68166a86334c2dc508a7f67e8ed3ef7b
David Mayer <david.mayer@cuanschutz.edu>
2025-06-09 07:37:31 -0600

    enable example

--- R/GXwasR_main_functions.R
+++ R/GXwasR_main_functions.R
@@ -499,7 +499,6 @@ AncestryCheck <-
 #' @export
 #'
 #' @examples
-#' \donttest{
 #' DataDir <- system.file("extdata", package = "GXwasR")
 #' ResultDir <- tempdir()
 #' finput <- "GXwasR_example"
@@ -539,7 +538,6 @@ AncestryCheck <-
 #'   flm_basis_function, flm_num_basis, flm_poly_order, flip_genotypes,
 #'   omit_linear_variant
 #' )
-#' }
 
 TestXGene <- function(DataDir,
                       ResultDir = tempdir(),
